522e54aafa601931cc6b0c364deeac75d3e36533
RazanHammad97 <razanhammadq@gmail.com>
2023-03-06 23:23:01 +0200

    add commit

--- lib/controllers/constants.dart
+++ lib/controllers/constants.dart
@@ -5,6 +5,7 @@ String baseUrl ="https://palmail.betweenltd.com/api";
 String loginEndPoint = "login";
 String registerEndpoint ="register";
 String logOutEndPoint="logout";
+
 String createCategoryEndPoint = "mails";
 Map<String,String>? headers = {
   "Content-Type": "application/json",
